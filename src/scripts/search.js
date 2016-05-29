09593d2dad5eb8c120f9bdab4eaf399c68e3714b
David Putney <david@davidputney.com>
2016-05-29 09:26:57 -0400

    Generic function to set data in a firebase database. Takes a data set and a child as arguments. Includes success / fail callback.

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,6 +1,20 @@
 var searchFunctions = {
   initialize: function() {
     console.log('search');
+  firebaseSet: function(dataSet, child) {
+    var self=this;
+
+    var entries = self.myFirebaseRef.child(child);
+
+    entries.set( dataSet,
+    function (error) {
+      if (error) {
+        console.log('error');
+      } else {
+        console.log('saved');
+      }
+    });
+  },
   getJSON: function(url) {
     var self=this;
 
