d1a8d95c8ad47686ce172444a080b1eb467bfb6d
David Putney <david@davidputney.com>
2016-05-29 09:24:36 -0400

    Generic function to get json from server using promises. Takes url as an argument.

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,5 +1,19 @@
 var searchFunctions = {
   initialize: function() {
     console.log('search');
+  getJSON: function(url) {
+    var self=this;
+
+    var p = new Promise (function(resolve, reject) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("GET", url);
+      xmlhttp.send();
+      xmlhttp.onload = function() {
+        xmlhttp.readyState == 4 && xmlhttp.status == 200
+        ? resolve(JSON.parse(xmlhttp.responseText))
+        : reject('fail');
+      };
+    });
+    return p;
   }
 };
