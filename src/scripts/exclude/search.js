30862b2dabe5085a748605244561f9b25f3304eb
David Putney <david@davidputney.com>
2016-06-19 18:36:00 -0400

    Adds initializer for listeners.

--- src/scripts/exclude/search.js
+++ src/scripts/exclude/search.js
@@ -1,7 +1,11 @@
 var searchFunctions = {
   initialize: function() {
     console.log('search works');
+  initializeListeners: function() {
+    var self=this;
+    var submitBtn = document.querySelector('#submit-search');
 
 
+  },
   }
 };
