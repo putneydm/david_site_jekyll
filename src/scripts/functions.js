8addd7ea00c0088e90b2c7a09f8aca22a67e1661
David Putney <david@davidputney.com>
2016-08-21 11:23:21 -0400

    Swaps js function that detects whether and item has scrolled into the viewport.

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -549,21 +549,30 @@ var pageFunctions = {
       self.removeShitTimer(topNav, 'nav-list--close', 500);
     }
   },
-  isElementVisible: function(el) {
-    var rect   = el.getBoundingClientRect(),
-      vWidth   = window.innerWidth || document.documentElement.clientWidth,
-      vHeight  = window.innerHeight || document.documentElement.clientHeight,
-      efp      = function (x, y) { return document.elementFromPoint(x, y); };
-    // Return false if it's not in the viewport
-    if (rect.right < 0 || rect.bottom < 0 || rect.left > vWidth || rect.top > vHeight) {
-      return false;
-      }
-    // Return true if any of its four corners are visible
-    return (
-        el.contains(efp(rect.left,  rect.top)) ||  el.contains(efp(rect.right, rect.top)) ||  el.contains(efp(rect.right, rect.bottom)) ||  el.contains(efp(rect.left,  rect.bottom)) );
-  },
+  // isElementVisible: function(el) {
+  //   var rect   = el.getBoundingClientRect(),
+  //     vWidth   = window.innerWidth || document.documentElement.clientWidth,
+  //     vHeight  = window.innerHeight || document.documentElement.clientHeight,
+  //     efp      = function (x, y) { return document.elementFromPoint(x, y); };
+  //   // Return false if it's not in the viewport
+  //   if (rect.right < 0 || rect.bottom < 0 || rect.left > vWidth || rect.top > vHeight) {
+  //     return false;
+  //     }
+  //   // Return true if any of its four corners are visible
+  //   return (
+  //       el.contains(efp(rect.left,  rect.top)) ||  el.contains(efp(rect.right, rect.top)) ||  el.contains(efp(rect.right, rect.bottom)) ||  el.contains(efp(rect.left,  rect.bottom)) );
+  // },
   getScrollPosition: function () {
     var scrollPosition = window.scrollY;
     return scrollPosition;
-  }
+  },
+  isElementVisible: function ( elem ) {
+      var distance = elem.getBoundingClientRect();
+      return (
+          distance.top >= 0 &&
+          distance.left >= 0 &&
+          distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+}
 };
