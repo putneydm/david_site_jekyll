cc954b878513a8218c5f7e12bb1d2d913549dcf0
David Putney <david@davidputney.com>
2016-09-06 17:38:42 -0400

    Removes old function that was replaced with a newer one.

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -587,19 +587,6 @@ var pageFunctions = {
       self.removeShitTimer(topNav, 'nav-list--close', 500);
     }
   },
-  // isElementVisible: function(el) {
-  //   var rect   = el.getBoundingClientRect(),
-  //     vWidth   = window.innerWidth || document.documentElement.clientWidth,
-  //     vHeight  = window.innerHeight || document.documentElement.clientHeight,
-  //     efp      = function (x, y) { return document.elementFromPoint(x, y); };
-  //   // Return false if it's not in the viewport
-  //   if (rect.right < 0 || rect.bottom < 0 || rect.left > vWidth || rect.top > vHeight) {
-  //     return false;
-  //     }
-  //   // Return true if any of its four corners are visible
-  //   return (
-  //       el.contains(efp(rect.left,  rect.top)) ||  el.contains(efp(rect.right, rect.top)) ||  el.contains(efp(rect.right, rect.bottom)) ||  el.contains(efp(rect.left,  rect.bottom)) );
-  // },
   getScrollPosition: function () {
     return scrollPosition = window.scrollY;
   },
