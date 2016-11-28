d4786781f81e687f03a3ca3ca763766e0bd2d350
David Putney <david@davidputney.com>
2016-11-28 08:44:50 -0500

    Gets and parses the variable from the URL.

--- src/scripts/exclude/search.js
+++ src/scripts/exclude/search.js
@@ -462,6 +462,18 @@ var searchFunctions = {
       self.windowWidth = document.documentElement.clientWidth;
     }
   },
+  getQueryVariable: function(variable) {
+    var self=this;
+
+     var query = window.location.search.substring(1);
+     var vars = query.split("&");
+
+     for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+      if(pair[0] == variable){return pair[1].replace(/%20/g, ' ');}
+     }
+     return(false);
+  },
   fooBar: [
         {
           "title": "this is a headline", "post": "this is a post"
