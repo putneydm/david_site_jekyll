66cebc11166d128aacf014e437ea0211729a2317
David Putney <david@davidputney.com>
2021-12-15 15:53:46 -0500

    Changes to a new gulp task for making svg icon sprites.

--- gulpfile.babel.js/visuals/portfolioSVG.js
+++ gulpfile.babel.js/visuals/portfolioSVG.js
@@ -2,9 +2,10 @@ const { src, dest } = require('gulp');
 import { paths } from "../variables"
 
 //svg
-const svgstore = require('gulp-svgstore');
-const svgmin = require('gulp-svgmin');
-const rename = require('gulp-rename');
+// const svgstore = require('gulp-svgstore');
+// const svgmin = require('gulp-svgmin');
+const svgSprite = require('gulp-svg-sprites');
+// const rename = require('gulp-rename');
 
 const {
     portfolioSVG: {
@@ -13,16 +14,19 @@ const {
     }
 } = paths;
 
+const config = {
+  mode: "symbols", 
+  preview: false, 
+  baseSize: 18,
+  svg: {
+    symbols: "port_svgsprite.svg"
+},
+}
 // creates svg sprite and moves it to testing and dist
 function portfolioSVG() {
     return src(input)
-        .pipe(svgmin())
-        .pipe(svgstore())
-        .pipe(rename({
-            basename: 'port_svgsprite',
-            extname: '.svg'
-        }))
-        .pipe(dest(output))
+    .pipe(svgSprite(config))
+    .pipe(dest(output))
 }
 
 exports.portfolioSVG = portfolioSVG;
--- gulpfile.babel.js/visuals/svg.js
+++ gulpfile.babel.js/visuals/svg.js
@@ -2,9 +2,7 @@ const { src, dest } = require('gulp');
 import { paths } from "../variables"
 
 //svg
-const svgstore = require('gulp-svgstore');
-const svgmin = require('gulp-svgmin');
-const rename = require('gulp-rename');
+const svgSprite = require('gulp-svg-sprites');
 
 const {
     svg: {
@@ -14,15 +12,19 @@ const {
 } = paths;
 
 // creates svg sprite and moves it to testing and dist
-function svg() {
-    return src(input)
-        .pipe(svgmin())
-        .pipe(svgstore())
-        .pipe(rename({
-            basename: 'svgsprite',
-            extname: '.svg'
-        }))
-        .pipe(dest(output))
-}
+const config = {
+    mode: "symbols", 
+    preview: false, 
+    baseSize: 18,
+    svg: {
+      symbols: "svgsprite.svg"
+  },
+  }
+  // creates svg sprite and moves it to testing and dist
+  function svg() {
+      return src(input)
+      .pipe(svgSprite(config))
+      .pipe(dest(output))
+  }
 
 exports.svg = svg;
